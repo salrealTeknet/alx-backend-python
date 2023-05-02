06057b41b009ad40c56c863de52296d7d0e6f57b
salrealTeknet <salihuabduljawwad.a@gmail.com>
2023-05-02 22:21:20 +0000

    Random

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
+""" The basics of async  """
 import asyncio
 import random
 
 async def wait_random(max_delay: int = 10) -> float:
-    delay = random.uniform(0, max_delay)
-    await asyncio.sleep(delay)
-   return delay
+     random_float = random.uniform(0, max_delay)
+    await asyncio.sleep(random_float)
+    return random_float
