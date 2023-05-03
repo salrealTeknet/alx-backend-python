9cb80935b0fb677ba6d9a488882715f1e2acf755
salrealTeknet <salihuabduljawwad.a@gmail.com>
2023-05-03 22:47:00 +0000

    Measure Asynch compreh.

---
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+""" Measures the runtine of Async comprehension """
+from asyncio import gather
+import time
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime() -> float:
+    """
+    a measure_runtime coroutine that will execute async_comprehension four
+    times in parallel using asyncio.gather
+
+    """
+    s = time.perf_counter()
+    await gather(
+        async_comprehension(),
+        async_comprehension(),
+        async_comprehension(),
+        async_comprehension()
+    )
+    elapsed = time.perf_counter() - s
+    return elapsed
+
